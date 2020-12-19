3e35b41c0b9c6f2dfb39758bdeb131a3d058751a
pv-frota <pvjacinto8@gmail.com>
2020-12-19 14:50:47 -0300

    feed controller template

--- lib/locator/locator.dart
+++ lib/locator/locator.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:tracksfer/screens/feed/feed_controller.dart';
 import 'package:tracksfer/screens/groups/list/group_list_controller.dart';
 import 'package:tracksfer/services/navigation/navigation_controller.dart';
 
@@ -7,4 +8,5 @@ final G = GetIt.instance;
 void initLocator() {
   G.registerSingleton(NavigationController());
   G.registerSingleton(GroupListController());
+  G.registerSingleton(FeedController());
 }
